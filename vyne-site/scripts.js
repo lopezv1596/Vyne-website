da85aae9199ab17f95dda22c1a9e8cc52b44f5bb
lopezv1596 <Vincentlopez461@gmail.com>
2025-03-11 18:26:25 -0700

    Updating Vyne website design

---
+++ vyne-site/scripts.js
@@ -0,0 +1,5 @@
+// GSAP Animations
+gsap.from(".animate-text-fade", { opacity: 0, y: 20, duration: 1, ease: "power3.out" });
+gsap.from(".animate-button-fade", { opacity: 0, y: 20, delay: 0.5, duration: 1, ease: "power3.out" });
+gsap.from(".animate-feature-fade-in", { opacity: 0, y: 20, delay: 1, duration: 1, stagger: 0.2, ease: "power3.out" });
+
